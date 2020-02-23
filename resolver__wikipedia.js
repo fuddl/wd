881f431369c6730283bd8038f4556563a843dfba
Alex <alex@Alexs-MBP.fritz.box>
2020-02-23 19:19:19 +0100

    added wikipedia resolver

---
+++ resolver__wikipedia.js
@@ -0,0 +1,11 @@
+resolvers.wikipedia = {
+	urlMatrch: function(location) {
+		return location.href.match(/^https:\/\/[\w]+(\.m)?\.wik(ipedia|iquote|tionary|isource)\.org\/wiki\/[\w%\:\(\)]+$/) !== null;
+	},
+	getEntityId: async function() {
+		let meta = document.querySelector('script[type="application/ld+json"]');
+		let metaData = JSON.parse(meta.innerText);
+		let sameAs = metaData.sameAs;
+		return sameAs.match(/http:\/\/www.wikidata.org\/entity\/(Q\d+)/)[1];
+	}
+};
