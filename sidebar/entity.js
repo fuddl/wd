704984c14c4cf1e043268fb0a9c3a84e22f10b42
Alex <alex@Alexs-MBP.fritz.box>
2020-02-25 21:02:31 +0100

    misc

--- sidebar/entity.js
+++ sidebar/entity.js
@@ -5,6 +5,16 @@ if (window.location.search) {
 	updateView(currentEntity);
 }
 
+function getLink(entityId) {
+	let ns = entityId.charAt(0);
+	let prefixes = {
+		Q: 'https://www.wikidata.org/wiki/',
+		P: 'https://www.wikidata.org/wiki/Property:',
+		L: 'https://www.wikidata.org/wiki/Lexeme:',
+	}
+	return prefixes[ns] + entityId;
+}
+
 function getValueByLang(e, key, fallback) {
 	if (!fallback) {
 		let fallback = '';
@@ -147,6 +157,7 @@ function updateView(url) {
 				let id = placeholder.getAttribute('data-entity');
 				let entity = await wikidataGetEntity(id);
 				let link = document.createElement('a');
+				link.setAttribute('href', getLink(id));
 				link.innerText = getValueByLang(entity[id], 'labels', id);
 				placeholder.parentNode.replaceChild(link, placeholder);
 			})();
