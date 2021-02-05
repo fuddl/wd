a82f9d722e6f2b46a87003dc48a90e494613932d
Markus Göllnitz <camelcasenick@bewares.it>
2021-02-05 23:59:02 +0100

    make getAutodesc more resilient to outages of its service

--- sidebar/get-autodesc.js
+++ sidebar/get-autodesc.js
@@ -1,10 +1,12 @@
 async function getAutodesc(id) {
-	const lang = navigator.language.substr(0,2);
-	let response = await fetch('https://tools.wmflabs.org/autodesc/?q=' + id + '&lang=' + lang + '&mode=short&links=text&redlinks=&format=json');
-	let json = JSON.parse(await response.text());
-	if (!json.result.match(/<i>/)) {
-		return json.result;
-	} else {
+	try {
+		const lang = navigator.language.substr(0,2);
+		let response = await fetch('https://autodesc.toolforge.org/?q=' + id + '&lang=' + lang + '&mode=short&links=text&redlinks=&format=json');
+		let json = await response.json();
+		if (!json.result.match(/<i>/)) {
+			return json.result;
+		}
+	} finally {
 		return '???';
 	}
 }
