8c997bed7e1092395147566ba8b07d1d71db1066
fuddl <alxndr@mailbox.org>
2021-11-07 17:02:18 +0100

    also find urls without slash suffix

--- content/resolver__url.js
+++ content/resolver__url.js
@@ -10,19 +10,30 @@ const url = {
 		}
 	},
 	getEntityId: async function(location) {
+		const href = location.href;
+		const hrefNoSlash = href.replace(/\/$/, '');
 		let query = `
 			SELECT ?item {
 			  {
-			    ?item wdt:P953 <${ location.href }>.
+			    ?item wdt:P953 <${ href }>.
 			  } UNION {
-			    ?item wdt:P973 <${ location.href }>.
+			    ?item wdt:P973 <${ href }>.
 			  } UNION {
-			    ?item wdt:P856 <${ location.href }>.
+			    ?item wdt:P856 <${ href }>.
 			  } UNION {
-			    ?item wdt:P2699 <${ location.href }>.
+			    ?item wdt:P2699 <${ href }>.
+			  } UNION {
+			    ?item wdt:P953 <${ hrefNoSlash }>.
+			  } UNION {
+			    ?item wdt:P973 <${ hrefNoSlash }>.
+			  } UNION {
+			    ?item wdt:P856 <${ hrefNoSlash }>.
+			  } UNION {
+			    ?item wdt:P2699 <${ hrefNoSlash }>.
 			  }
 			}
 		`;
+		console.debug(query);
 		let entity = await sparqlQuery(query);
 		if (entity[0]) {
 			let entityId = entity[0].item.value.match(/https?:\/\/www\.wikidata\.org\/entity\/(Q\d+)/)[1]
