27727bbf928c42181ca142a8ffdd83e41ceae573
Vladyslav Sitalo <sitalov@amazon.com>
2022-03-01 21:33:07 -0800

    Revert changes breaking FF

--- src/background/navigation.ts
+++ src/background/navigation.ts
@@ -4,16 +4,13 @@ import * as browser from "webextension-polyfill"
  * Provides an abstraction over switching iframe & actual sidebar state
  */
 export const setSidebarUrl = async (tabId, url) => {
-    if ('sidebarAction' in browser) {
-        await browser.sidebarAction.setPanel({
-            tabId,
-            panel: url,
-        });
-    } else {
-        await browser.tabs.sendMessage(tabId, {
-            type: 'update-panel-url',
-            url,
-        })  
-    }
+    await browser.tabs.sendMessage(tabId, {
+        type: 'update-panel-url',
+        url
+    })
 
+    await browser.sidebarAction?.setPanel({
+        tabId,
+        panel: url,
+    })
 }
