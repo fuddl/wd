2f09e15ac1aa5a8748834532338a674e822c421d
Blank <blank@Alexs-MacBook-Pro.fritz.box>
2022-07-09 20:33:30 +0200

    bugix

--- src/sidebar/components/ruby/ruby.tpl.js
+++ src/sidebar/components/ruby/ruby.tpl.js
@@ -1,13 +1,19 @@
 const ruby = (fragments) => {
 	const ruby = document.createElement('ruby');
-	for (fragment of fragments) {
+	for (const fragment of fragments) {
 		if (fragment.w) {
 			ruby.appendChild(document.createTextNode(fragment.w))
 		}
 		if (fragment.r) {
+			const start = document.createElement('rp')
+			start.innerText = '('
+			const end = document.createElement('rp')
+			end.innerText = ')'
 			const t = document.createElement('rt')
 			t.innerText = fragment.r
+			ruby.appendChild(start)
 			ruby.appendChild(t)
+			ruby.appendChild(end)
 		}
 	}
 
