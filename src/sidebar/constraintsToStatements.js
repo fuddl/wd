66ee449531ab4b88220b019ce1a050e55014fd36
Blank <blank@Alexs-MacBook-Pro.fritz.box>
2022-08-21 15:23:22 +0200

    bugfix

--- src/sidebar/constraintsToStatements.js
+++ src/sidebar/constraintsToStatements.js
@@ -37,108 +37,107 @@ function constraintsToStatements(prop, contraints, propform, classes) {
 		const contraintType = contraint.mainsnak.datavalue.value.id;
 		switch (contraintType) {
 			case 'Q21503250': // type constraint
-				const requiresInstanceOf = contraint?.qualifiers?.P2309[0]?.datavalue?.value?.id === 'Q21503252'
-				const requiresInstanceOrSubclassOf = contraint?.qualifiers?.P2309[0]?.datavalue?.value?.id === 'Q30208840'
-				if (requiresInstanceOf || requiresInstanceOrSubclassOf) {
-					let value = null;
-					let check = null;
+				const requiresInstanceOrSubclassOf = contraint?.qualifiers?.P2309?.[0]?.datavalue?.value?.id === 'Q30208840'
+				let value = null;
+				let check = null;
 
-					let proposedClasses = contraint.qualifiers.P2308.map((delta) => {
-						return delta.datavalue.value.id;
-					});
-					// if one of the proposed classes is already there, we don't need
-					// to go any further.
-					if (classes.filter(value => proposedClasses.includes(value)).length > 0) {
-						continue;
+				let proposedClasses = contraint.qualifiers.P2308.map((delta) => {
+					return delta.datavalue.value.id;
+				});
+				// if one of the proposed classes is already there, we don't need
+				// to go any further.
+				if (classes.filter(value => proposedClasses.includes(value)).length > 0) {
+					continue;
+				}
+
+				(async () => {
+					const options = []
+
+					for (let entity of contraint?.qualifiers?.P2308) {
+						options.push(entity.datavalue?.value)
 					}
 
-					(async () => {
-						const options = []
-
-						for (let entity of contraint?.qualifiers?.P2308) {
-							options.push(entity.datavalue?.value)
-						}
-
-						if (requiresInstanceOrSubclassOf) {
-							const query = `
-								SELECT ?s WHERE {
-									{ 	${
-											options.map((option) => { return `?subclass wdt:P279 + wd:${option.id}.` })
-												.join('} UNION {')
-										}
+					if (requiresInstanceOrSubclassOf) {
+						const query = `
+							SELECT ?s WHERE {
+								{ 	${
+										options.map((option) => { return `?subclass wdt:P279 + wd:${option.id}.` })
+											.join('} UNION {')
 									}
-									BIND (REPLACE(STR(?subclass), 'http://www.wikidata.org/entity/', '') AS ?s)
 								}
-								LIMIT 300
-							`
-							let result = await sparqlQuery(query);
-							if (result[0]) {
-								let more = result.map((i) => {
-										return {
-											id: i.s.value,
-											'numeric-id': parseInt(i.s.value.replace('^\w', '')),
-										}
-									}
-								)
-								options.push(...more)
+								BIND (REPLACE(STR(?subclass), 'http://www.wikidata.org/entity/', '') AS ?s)
 							}
-						}
-
-						if (options.length != 0) {
-							if (options.length > 1) {
-
-								value = document.createElement('select');
-								value.setAttribute('name', prop + 'Q21503250');
-								let emptyOption = document.createElement('option');
-								value.appendChild(emptyOption);
-								for (let entity of options) {
-									let option = templates.placeholder({
-										tag: 'option',
-										entity: entity.id,
-										type: 'option',
-									});
-									value.appendChild(option);
-									let job = {
-										type: 'set_claim',
-										verb: 'P31',
-										object: {
-											'entity-type': "item",
-											'numeric-id': entity['numeric-id'],
-										},
+							LIMIT 300
+						`
+						console.debug(query)
+						let result = await sparqlQuery(query);
+						console.debug(result)
+						if (result[0]) {
+							let more = result.map((i) => {
+									return {
+										id: i.s.value,
+										'numeric-id': parseInt(i.s.value.replace('^\w', '')),
 									}
-									option.setAttribute('value', JSON.stringify(job))
 								}
-							} else {
-								check = document.createElement('input');
-								check.setAttribute('type', 'checkbox');
-								check.setAttribute('name', prop + 'Q21503250');
+							)
+							options.push(...more)
+						}
+					}
+
+					if (options.length != 0) {
+						if (options.length > 1) {
+
+							value = document.createElement('select');
+							value.setAttribute('name', prop + 'Q21503250');
+							let emptyOption = document.createElement('option');
+							value.appendChild(emptyOption);
+							for (let entity of options) {
+								let option = templates.placeholder({
+									tag: 'option',
+									entity: entity.id,
+									type: 'option',
+								});
+								value.appendChild(option);
 								let job = {
 									type: 'set_claim',
 									verb: 'P31',
 									object: {
 										'entity-type': "item",
-										'numeric-id': options[0]['numeric-id'],
+										'numeric-id': entity['numeric-id'],
 									},
 								}
-								check.setAttribute('value', JSON.stringify(job))
-								check.checked = true;
-								value = templates.placeholder({
-									entity: options[0].id,
-								})
+								option.setAttribute('value', JSON.stringify(job))
 							}
-							let instanceOfPreview = templates.remark({
-								sortKey: 'P31',
-								check: check ? check : null,
-								prop: templates.placeholder({
-									entity: 'P31',
-								}),
-								vals: addConstraintComment(value, contraintType, prop),
-							});
-
-							propform.insertBefore(instanceOfPreview, propform.firstChild);
+						} else {
+							check = document.createElement('input');
+							check.setAttribute('type', 'checkbox');
+							check.setAttribute('name', prop + 'Q21503250');
+							let job = {
+								type: 'set_claim',
+								verb: 'P31',
+								object: {
+									'entity-type': "item",
+									'numeric-id': options[0]['numeric-id'],
+								},
+							}
+							check.setAttribute('value', JSON.stringify(job))
+							check.checked = true;
+							value = templates.placeholder({
+								entity: options[0].id,
+							})
 						}
-					})()
-				}
+						let instanceOfPreview = templates.remark({
+							sortKey: 'P31',
+							check: check ? check : null,
+							prop: templates.placeholder({
+								entity: 'P31',
+							}),
+							vals: addConstraintComment(value, contraintType, prop),
+						});
+
+						propform.insertBefore(instanceOfPreview, propform.firstChild);
+					}
+				})()
 				break;
 			case 'Q21503247': // item-requires-statement constraint
 				if (contraint?.qualifiers?.P2305) {
