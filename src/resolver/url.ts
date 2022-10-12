d168975493886819db846cbe9d333950cf6462de
Blank <blank@Alexs-MacBook-Pro.fritz.box>
2022-10-12 18:34:06 +0200

    adding permutations

--- src/resolver/url.ts
+++ src/resolver/url.ts
@@ -1,9 +1,10 @@
 import { sparqlQuery } from '../sqarql-query.js'
-import {Resolver} from './types'
+import { Resolver } from './types'
+import binaryVariations from 'binary-variations'
 
 const url: Resolver = {
 	id: 'url',
-	props: ['P953', 'P973', 'P856', 'P2699'],
+	props: ['P953', 'P973', 'P856', 'P2699', 'P1581'],
 	applicable: async function(location) {
 		if (location.href === window.location.href) {
 			return [{
@@ -14,31 +15,34 @@ const url: Resolver = {
 		}
 	},
 	fuzziness: {
-		slashNoSlash: [
-			(url) => url.endsWith('/') ? url : `${url}/`,
-			(url) => !url.endsWith('/') ? url : url.replace(/\/$/, ''),
-		],
-		secureNoSecure: [
-			(url) => url.startsWith('http://') ? url.replace(/^http:\/\//, 'https://') : url,
-			(url) => url.startsWith('https://') ? url : url.replace(/^https:\/\//, 'http://'),
-		],
-		wwwNoWww: [
-			(url) => url.match(/https?:\/\/www\./) ? url.replace(/^(http:\/\/)www\./, '$1') : null,
-		],
+		noIndexHtml: (url) => url.replace(/\/index\.html?$/, ''),
+		trailingSlash: (url) => url.endsWith('/') ? url : `${url}/`,
+		noTrailingSlash: (url) => !url.endsWith('/') ? url : url.replace(/\/$/, ''),
+		secure: (url) => url.startsWith('http://') ? url.replace(/^http:\/\//, 'https://') : url,
+		noScecure: (url) => url.startsWith('https://') ? url.replace(/^https:\/\//, 'http://') : url,
+		noWww: (url) => url.match(/https?:\/\/www\./) ? url.replace(/^(http:\/\/)www\./, '$1') : url,
 	},
 	getEntityId: async function(location) {
 		let href = location.href
 
-		let groups = []
+		let hrefs = [href]
 
+		const fuzzyPermutation = binaryVariations(Object.keys(this.fuzziness))
+
+		for (const fuzzy of fuzzyPermutation) {
+			let variation = href
+			for (const fuzz of fuzzy) {
+				variation = this.fuzziness[fuzz](variation)
+			}
+			if (variation && !hrefs.includes(variation)) {
+				hrefs.push(variation)	
+			}
+		}
+
+		let groups = []
 		for (const prop of this.props) {
-			for (const fuzz in this.fuzziness) {
-				for (const variation of this.fuzziness[fuzz]) {
-					const fuzzyUrl = variation(href)
-					if (fuzzyUrl) {
-						groups.push(`?item wdt:${prop} <${ fuzzyUrl }>.`)
-					}
-				}
+			for (const fuzzyHref of hrefs) {
+				groups.push(`?item wdt:${prop} <${ fuzzyHref }>.`)
 			}
 		}
 
@@ -49,6 +53,7 @@ const url: Resolver = {
 			  }
 			}
 		`
+
 		const entity = await sparqlQuery(query)
 		if (entity[0]) {
 			const entityId = entity[0].item.value.match(/https?:\/\/www\.wikidata\.org\/entity\/(Q\d+)/)[1]
