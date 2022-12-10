48ac6cfe15fc88bbe0fcc7cfc397272c6de6d01f
Blank <blank@Alexs-MacBook-Pro.fritz.box>
2022-12-10 12:15:24 +0100

    adding a tiny delay to the autocomplete

--- src/sidebar/components/join/join.tpl.js
+++ src/sidebar/components/join/join.tpl.js
@@ -38,49 +38,54 @@ const join = (vars) => {
 
 	wrapper.appendChild(style);
 
+	var timeout = null
 	let updateList = async () => {
-		const lang = navigator.language.substr(0,2);
-		let suggestions = await wikidataAutocomplete(humanField.value, lang, vars.scope);
-		proposals.innerHTML = '';
+		clearTimeout(timeout)
+		timeout = setTimeout(async () => {
 
-		if (vars.scope === 'item') {
-			suggestions.push({
-				label: humanField.value,
-				title: 'CREATE',
-				description: 'CREATE',
-			});
-		}
+			const lang = navigator.language.substr(0,2);
+			let suggestions = await wikidataAutocomplete(humanField.value, lang, vars.scope);
+			proposals.innerHTML = '';
 
-		if (suggestions) { 
-			for (let suggestion of suggestions) {
-				let item = document.createElement('button');
-				item.setAttribute('tabindex', '0');
-				item.classList.add('join__proposal');
-				item.innerText = suggestion.label;
-				let desc = document.createElement('div');
-				item.appendChild(desc);
-				desc.classList.add('join__proposal__desc');
-				desc.innerText = suggestion.description ? suggestion.description : '█████ █ ██████ ████ ████';
-				if (!suggestion.description) {
-					desc.classList.add('join__proposal__desc--placeholder');
-				}
-
-				proposals.appendChild(item);
-				item.setAttribute('data-entity', suggestion.title.replace(/^\w+\:/, ''));
-				item.setAttribute('data-label', suggestion.label);
-				item.addEventListener('click', async () => {
-					humanField.value = item.getAttribute('data-label');
-					idField.value = item.getAttribute('data-entity');
-					proposals.innerHTML = '';
-					wrapper.setAttribute('data-selected-entity', item.getAttribute('data-entity'))
-					wrapper.setAttribute('data-selected-label', item.getAttribute('data-label'))
+			if (vars.scope === 'item') {
+				suggestions.push({
+					label: humanField.value,
+					title: 'CREATE',
+					description: 'CREATE',
 				});
 			}
-		}
-		for (let placeholder of proposals.querySelectorAll('.join__proposal__desc--placeholder')) {	
-			let entityWithoutDesc = placeholder.parentNode.getAttribute('data-entity');
-			placeholder.innerText = await getAutodesc(entityWithoutDesc);
-		}
+
+			if (suggestions) { 
+				for (let suggestion of suggestions) {
+					let item = document.createElement('button');
+					item.setAttribute('tabindex', '0');
+					item.classList.add('join__proposal');
+					item.innerText = suggestion.label;
+					let desc = document.createElement('div');
+					item.appendChild(desc);
+					desc.classList.add('join__proposal__desc');
+					desc.innerText = suggestion.description ? suggestion.description : '█████ █ ██████ ████ ████';
+					if (!suggestion.description) {
+						desc.classList.add('join__proposal__desc--placeholder');
+					}
+
+					proposals.appendChild(item);
+					item.setAttribute('data-entity', suggestion.title.replace(/^\w+\:/, ''));
+					item.setAttribute('data-label', suggestion.label);
+					item.addEventListener('click', async () => {
+						humanField.value = item.getAttribute('data-label');
+						idField.value = item.getAttribute('data-entity');
+						proposals.innerHTML = '';
+						wrapper.setAttribute('data-selected-entity', item.getAttribute('data-entity'))
+						wrapper.setAttribute('data-selected-label', item.getAttribute('data-label'))
+					});
+				}
+			}
+			for (let placeholder of proposals.querySelectorAll('.join__proposal__desc--placeholder')) {	
+				let entityWithoutDesc = placeholder.parentNode.getAttribute('data-entity');
+				placeholder.innerText = await getAutodesc(entityWithoutDesc);
+			}
+		}, 100)
 	}
 
 	browser.runtime.onMessage.addListener(async (data, sender) => {
