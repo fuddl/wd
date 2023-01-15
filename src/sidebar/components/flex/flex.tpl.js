1513f9745399341064435a8eaa994b8fe4494c63
Blank <blank@Alexs-MacBook-Pro.fritz.box>
2023-01-15 11:21:26 +0100

    split spelling variant tables

--- src/sidebar/components/flex/flex.tpl.js
+++ src/sidebar/components/flex/flex.tpl.js
@@ -1,8 +1,11 @@
 import { placeholder } from "../placeholder/placeholder.tpl.js";
 import { rubifyLemma } from '../../rubifyLemma.js';
 import { requreStylesheet } from '../require-styleheet.js'
+import intersection from 'array-intersection'
 
 import rules from "./flex-rules.yml";
+
+const variantFeatures = ['Q115819543']
 
 function buildRepresentations(target, form, affix = {}, lexeme) {
 	
@@ -72,131 +75,154 @@ function buildAffix(target, affix, position = "after", lexeme) {
 }
 
 const flex = (vars) => {
-	let table = document.createElement("table");
-	table.classList.add("flex");
+	const output = document.createDocumentFragment()
 
 	let xAxis, yAxis;
 	let affixes = [];
 	let hiddenLabels = [];
 
-	for (const lang in rules) {
-		const ruleset = rules[lang];
-		if (ruleset.lang === vars.lang) {
-			for (const type in ruleset.categories) {
-				let wordType = ruleset.categories[type];
-				if (vars.category === wordType.category) {
-					// in case this word requires a certain gender which the
-					// lexeme does not have, continue to the next loop
-					if (wordType?.gender && vars.gender != wordType.gender) {
-						continue;
-					}
+	const formSets = {
+		'Q0': [],
+	}
+	for (const form of vars.forms) {
+		if (intersection(form.grammaticalFeatures, variantFeatures).length == 0) {
+			formSets.Q0.push(form)
+		}
+	}
 
-					let header = document.createElement("thead");
-					let hRow = document.createElement("tr");
-					table.appendChild(header);
-					header.appendChild(hRow);
-					const oneColumn = wordType?.xAxis ? false : true;
-					const oneRow = wordType?.yAxis ? false : true;
-					let tbody = document.createElement("tbody");
-					table.appendChild(tbody);
+	for (const feature of variantFeatures) {
+		for (const form of vars.forms) {
+			if (form?.grammaticalFeatures.includes(feature)) {
+				if (!formSets.hasOwnProperty(feature)) {
+					formSets[feature] = []
+				}
+				formSets[feature].push(form)
+			}
+		}
+	}
+	for (const set in formSets) {
+		let table = document.createElement("table");
+		table.classList.add("flex");
 
-					// the upper right column should be empty when there
-					// is more than one row and more than one column.
-					if (!oneColumn && !oneRow) {
-						let emptyHead = document.createElement("th");
-						hRow.appendChild(emptyHead);
-					}
+		for (const lang in rules) {
+			const ruleset = rules[lang];
+			if (ruleset.lang === vars.lang) {
+				for (const type in ruleset.categories) {
+					let wordType = ruleset.categories[type];
+					if (vars.category === wordType.category) {
+						// in case this word requires a certain gender which the
+						// lexeme does not have, continue to the next loop
+						if (wordType?.gender && vars.gender != wordType.gender) {
+							continue;
+						}
 
-					// draw the first cell in each row
-					for (let y in wordType.yAxis) {
-						wordType.yAxis[y].row = document.createElement("tr");
-						tbody.appendChild(wordType.yAxis[y].row);
-						const yFeatures = wordType.yAxis[y].features;
-						for (let feature of yFeatures) {
-							if (
-								!wordType.yAxis[y].hidden ||
-								!wordType.yAxis[y].hidden.includes(feature)
-							) {
-								let hCell = document.createElement("th");
-								hCell.appendChild(
-									placeholder({
-										entity: feature,
-									})
-								);
-								wordType.yAxis[y].row.appendChild(hCell);
+						let header = document.createElement("thead");
+						let hRow = document.createElement("tr");
+						table.appendChild(header);
+						header.appendChild(hRow);
+						const oneColumn = wordType?.xAxis ? false : true;
+						const oneRow = wordType?.yAxis ? false : true;
+						let tbody = document.createElement("tbody");
+						table.appendChild(tbody);
+
+						// the upper right column should be empty when there
+						// is more than one row and more than one column.
+						if (!oneColumn && !oneRow) {
+							let emptyHead = document.createElement("th");
+							hRow.appendChild(emptyHead);
+						}
+
+						// draw the first cell in each row
+						for (let y in wordType.yAxis) {
+							wordType.yAxis[y].row = document.createElement("tr");
+							tbody.appendChild(wordType.yAxis[y].row);
+							const yFeatures = wordType.yAxis[y].features;
+							for (let feature of yFeatures) {
+								if (
+									!wordType.yAxis[y].hidden ||
+									!wordType.yAxis[y].hidden.includes(feature)
+								) {
+									let hCell = document.createElement("th");
+									hCell.appendChild(
+										placeholder({
+											entity: feature,
+										})
+									);
+									wordType.yAxis[y].row.appendChild(hCell);
+								}
 							}
 						}
-					}
 
-					let iterator = oneColumn ? { one: true } : wordType.xAxis;
-					for (let x in iterator) {
-						let xFeatures = [];
-						if (!oneColumn) {
-							xFeatures = wordType.xAxis[x].features;
-							for (let feature of xFeatures) {
-								let hCell = document.createElement("th");
-								hCell.appendChild(
-									placeholder({
-										entity: feature,
-									})
-								);
-								hRow.appendChild(hCell);
+						let iterator = oneColumn ? { one: true } : wordType.xAxis;
+						for (let x in iterator) {
+							let xFeatures = [];
+							if (!oneColumn) {
+								xFeatures = wordType.xAxis[x].features;
+								for (let feature of xFeatures) {
+									let hCell = document.createElement("th");
+									hCell.appendChild(
+										placeholder({
+											entity: feature,
+										})
+									);
+									hRow.appendChild(hCell);
+								}
+							}
+
+							for (let y in wordType.yAxis) {
+								let yFeatures = wordType.yAxis[y].features;
+								let dCell = document.createElement("td");
+								for (let form of formSets[set]) {
+									if (
+										oneColumn ||
+										xFeatures.every((v) => form.grammaticalFeatures.includes(v))
+									) {
+										if (
+											yFeatures.every((v) => {
+												return form.grammaticalFeatures.includes(v);
+											})
+										) {
+											let affix = wordType?.affixes?.find((v) => {
+												if (
+													v.features.every((vv) =>
+														[...xFeatures, ...yFeatures].includes(vv)
+													)
+												) {
+													return v;
+												}
+											});
+											buildRepresentations(dCell, form, affix, vars);
+										}
+									}
+								}
+								wordType.yAxis[y].row.appendChild(dCell);
 							}
 						}
 
 						for (let y in wordType.yAxis) {
-							let yFeatures = wordType.yAxis[y].features;
-							let dCell = document.createElement("td");
-							for (let form of vars.forms) {
-								if (
-									oneColumn ||
-									xFeatures.every((v) => form.grammaticalFeatures.includes(v))
-								) {
-									if (
-										yFeatures.every((v) => {
-											return form.grammaticalFeatures.includes(v);
+							if (!oneRow) {
+								let row = document.createElement("tr");
+								tbody.appendChild(row);
+								if (oneRow) {
+									let hCell = document.createElement("th");
+									hCell.appendChild(
+										placeholder({
+											entity: y,
 										})
-									) {
-										let affix = wordType?.affixes?.find((v) => {
-											if (
-												v.features.every((vv) =>
-													[...xFeatures, ...yFeatures].includes(vv)
-												)
-											) {
-												return v;
-											}
-										});
-										buildRepresentations(dCell, form, affix, vars);
-									}
+									);
+									row.appendChild(hCell);
 								}
-							}
-							wordType.yAxis[y].row.appendChild(dCell);
-						}
-					}
-
-					for (let y in wordType.yAxis) {
-						if (!oneRow) {
-							let row = document.createElement("tr");
-							tbody.appendChild(row);
-							if (oneRow) {
-								let hCell = document.createElement("th");
-								hCell.appendChild(
-									placeholder({
-										entity: y,
-									})
-								);
-								row.appendChild(hCell);
-							}
-							for (let x in wordType.xAxis) {
-								if (wordType.xAxis[x].present) {
-									let dCell = document.createElement("td");
-									row.appendChild(dCell);
-									if (
-										form.grammaticalFeatures.includes(x) &&
-										(form.grammaticalFeatures.includes(y) || oneRow)
-									) {
-										buildRepresentations(dCell, form, {}, vars);
-										dCell.appendChild(formLink);
+								for (let x in wordType.xAxis) {
+									if (wordType.xAxis[x].present) {
+										let dCell = document.createElement("td");
+										row.appendChild(dCell);
+										if (
+											form.grammaticalFeatures.includes(x) &&
+											(form.grammaticalFeatures.includes(y) || oneRow)
+										) {
+											buildRepresentations(dCell, form, {}, vars);
+											dCell.appendChild(formLink);
+										}
 									}
 								}
 							}
@@ -205,9 +231,21 @@ const flex = (vars) => {
 				}
 			}
 		}
+		if (table.childNodes.length > 0) {
+			console.debug(set)
+			if (set != 'Q0') {
+				const heading = document.createElement('h3')
+				heading.appendChild(placeholder({
+					entity: set,
+				}))
+				output.appendChild(heading)
+			}
+			output.appendChild(table)
+		}
 	}
-	if (table.childNodes.length > 0) {
-		return table;
+
+	if (output.childNodes.length > 0) {
+		return output;
 	} else {
 		return false;
 	}
